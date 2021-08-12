3f9f6c733938e943a8ad61635d874f4306a4fb8c
maxProgrammer <maxwneto.poo@gmail.com>
2021-08-11 23:00:56 -0300

    Criados atributos de instância nome e idade.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    # atributos de instancia e objetos são criados
+    # através do método especial dunder init
+
+    def __init__(self, nome=None):
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Luciano')
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    p.nome = 'Renzo'
+    print(p.nome)
